9102f24afc21e6faa4e50e8e9d4cca7d0f09971b
Kevin Jacome <kjacomeg@est.ups.edu.ec>
2024-04-26 19:28:56 -0500

    Resolucion de ejercicios

--- ejercicio.py
+++ ejercicio.py
@@ -1,10 +1,15 @@
 # Ingrese una lista de compras: manzanas, plátanos, leche, pan
 # Los productos en la lista de compras son: ['manzanas', 'plátanos', 'leche', 'pan']
 
-compras = input("Ingrese una lista de compras: ")
-productos = compras.split(", ")
-print(f"Los productos en la lista de compras son: {productos}")
+# compras = input("Ingrese una lista de compras: ")
+# productos = compras.split(", ")
+# print(f"Los productos en la lista de compras son: {productos}")
 
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
-    return # Utiliza una función para convertir 
+    print(f"Los productos en la lista de compras son: {lista}")
+    return tuple(lista)# Utiliza una función para convertir
+
+lista = "manzanas, plátanos, leche, pan"
+productos = lista.split(", ")
+print(f"Los productos en la lista de compras convertidos en tupla son:", convertir_lista_a_tupla(productos))
